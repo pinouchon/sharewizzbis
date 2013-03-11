eb22bb7f671d135e8d1f4b5e6553513fcc678814
Benjamin <bcrouzier@sharewizz.com>
2013-03-11 12:20:28 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharewizzbis::Application.config.secret_token = 'aa24768c1602938acee9bc2cf0120a8e3d7b48aab1ba259a6383cdacd0d2ff9fc5f345a00a5c0e837cc65f8ce64d5c5480a1ddcf3967ed5d2a7c63b32a0d77dd'
